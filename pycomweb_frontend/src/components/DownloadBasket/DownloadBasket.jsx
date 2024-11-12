9d6bed23331e7848228d0bdf23733db367d36e9a
Matthew ONeill <matthew.oneill@brunel.ac.uk>
2024-11-12 20:27:15 +0000

    Added basket downloader and zip save
    WIP: Currently downloads alignment files, and puts them in them in a folder and zip the collection up together

--- pycomweb_frontend/src/components/DownloadBasket/DownloadBasket.jsx
+++ pycomweb_frontend/src/components/DownloadBasket/DownloadBasket.jsx
@@ -2,15 +2,52 @@ import React, { useContext } from 'react'
 import { RepositoryContext } from '../../context/RepositorContext';
 import { DownloadContext } from '../../context/DownloadContext';
 import useFetchProteinSummaryData from '../../customHooks/useFetchProteinSummaryData';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 function DownloadBasket() {
   const { basket, removeBasketItem, emptyBasket} = useContext(DownloadContext);
   const { proteinRepository } = useContext(RepositoryContext);
-  // const [downloadSummaryData, setDownloadSummaryData] = useState([]);
-  // const [loading, setLoading] = useState(true);
   
   const {loadingSummary} = useFetchProteinSummaryData(basket);
-  // const dataRequired = [SUMMARY_PROTEIN_DATA];
+
+  function getFormattedDate(date = new Date()) {
+    const year = date.toLocaleString('default', { year: 'numeric' });
+    const month = date.toLocaleString('default', { month: '2-digit' }).padStart(2, 0);
+    const day = date.toLocaleString('default', { day: '2-digit' }).padStart(2, 0);
+    const hour = date.toLocaleString('default', { hour: '2-digit'}).padStart(2, 0);
+    const minute = date.toLocaleString('default', { minute: '2-digit'}).padStart(2, 0);
+    const second = date.toLocaleString('default', { second: '2-digit'}).padStart(2, 0);
+    
+    const datePart = [year, month, day].join('-');
+    const timePart = [hour, minute, second].join('-');
+    return `${datePart}_${timePart}`;
+  }
+
+  const downloadAll = async () => {
+    const zipFileDate = getFormattedDate();
+    const zipFile = new JSZip();
+
+    await Promise.all(
+      basket.map(async (item) => {
+        //get alignment file
+        let url = `https://pycom.brunel.ac.uk/alignments/${item}.aln`
+        let filename = `${item}.aln`
+        const response = await fetch(url)
+        const blobData = await response.blob();
+        
+        //create a folder for the data
+        let folder = zipFile.folder(item);
+        //add file to the folder
+        folder.file(filename, blobData);
+      })
+    )
+
+    // save zip file to user computer
+    zipFile.generateAsync({type: 'blob'}).then((content) => {
+      saveAs(content, `PyComFiles_${zipFileDate}.zip`);
+    });
+  }
 
   return (
     <>
@@ -29,7 +66,7 @@ function DownloadBasket() {
               <>{proteinRepository[item]?.uniprot_id} {proteinRepository[item]?.summaryData?.protein_name || "no name"}&nbsp;<button onClick={()=> removeBasketItem(item)}>delete</button><br /></>
               </React.Fragment>
             ))}</div>
-            <div>Download All</div>
+            <button onClick={() => downloadAll()}>Download All</button>
             <br />
             <button onClick={()=> emptyBasket()}>Clear All</button>
             </>
