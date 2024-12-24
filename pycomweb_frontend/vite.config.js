f74dd5d762d5036723041803e94dd24dcf09faa5
hardeep0303 <hardeepkaur325@gmail.com>
2024-12-24 18:41:18 +0000

    debug environment on test server

--- pycomweb_frontend/vite.config.js
+++ pycomweb_frontend/vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), 'VITE_');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
+  base: env.VITE_BASE_URL || '/',
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
@@ -11,3 +13,8 @@ export default defineConfig({
   },
   plugins: [react()],
 })
+
+
+console.log("===============================")
+console.log(env.VITE_BASE_URL)
+console.log(process.env.NODE_ENV)
