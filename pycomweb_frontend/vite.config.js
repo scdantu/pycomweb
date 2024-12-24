3c1439766b3b208beb778747240e0ead6ba860d5
hardeep0303 <hardeepkaur325@gmail.com>
2024-12-24 16:58:10 +0000

    -react create  environment settings and vite config settings

--- pycomweb_frontend/vite.config.js
+++ pycomweb_frontend/vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api' : 'http://localhost:3000'
+  base: process.env.VITE_BASE_URL || '/',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
     }
   },
   plugins: [react()],
